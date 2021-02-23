cca4541d48fa127451c0ff3b467e001093f95493
Stephen Hurley <stephenhurley31@gmail.com>
2021-02-23 10:25:10 +0000

    corrected file path on line 6 of register.js

--- js/register.js
+++ js/register.js
@@ -3,7 +3,7 @@ window.onload = () => {
   
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
-               .register('./service_worker.js');
+               .register('./js/service_worker.js');
     }
   }
   
