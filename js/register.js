2efb39d9800e864101255267ab0a9c5ec9597d74
Stephen Hurley <stephenhurley31@gmail.com>
2021-02-23 10:41:09 +0000

    accidentally started with index not game, converted project accordingly

--- js/register.js
+++ js/register.js
@@ -6,15 +6,3 @@ window.onload = () => {
                .register('./js/service_worker.js');
     }
   }
-  
-function onPageLoad() {
-var href = window.location.href;
-   
-    var protocol = window.location.protocol;
-    
-    var host = window.location.host;
-    
-    var pathname = window.location.pathname;
-   
-    var search = window.location.search;
-}	
