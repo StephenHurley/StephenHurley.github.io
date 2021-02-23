4649a0feea2ae6816f9e90bf8ecd8802f882dddb
Stephen Hurley <stephenhurley31@gmail.com>
2021-02-23 10:23:31 +0000

    added register and service worker

---
+++ js/register.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+    'use strict';
+  
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+               .register('./service_worker.js');
+    }
+  }
+  
+function onPageLoad() {
+var href = window.location.href;
+   
+    var protocol = window.location.protocol;
+    
+    var host = window.location.host;
+    
+    var pathname = window.location.pathname;
+   
+    var search = window.location.search;
+}	
